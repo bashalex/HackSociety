cf182e712f04006d462c76c3cf051fff77738c14
Alex Bash <avbashlykov@gmail.com>
2016-12-10 01:12:10 +0300

    refactoring

--- questions.py
+++ questions.py
@@ -3,7 +3,8 @@ import random
 
 
 class Transaction:
-    def __init__(self, ticker, amount, client, second, direction):
+    def __init__(self, tx):
+        ticker, amount, client, second, direction = tx
         self.ticker = ticker
         self.amount = amount
         self.client = client
@@ -13,97 +14,153 @@ class Transaction:
 
 class Data:
     path = 'result.txt'
-    lineNum = 0
+    transactions = []
+    actors = {'Brad Pitt': "https://media1.popsugar-assets.com/files/thumbor/9xeS3fpQc1QdvdtpD5I4grLYMI4/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2012/09/36/2/192/1922398/2540217bec064a5c_71841717_10/i/Brad-Pitt-got-handsy-2006-press-conference-Babel.jpg",
+              'George C. Scott': "http://movieactors.com/photos-stars/george-c-scott-changeling-808.jpg",
+              'Jude Law': "http://www.kino-teatr.ru/news/3452/44235.jpg",
+              'Mel Gibson': "http://img.cinemablend.com/cb/e/5/1/b/7/5/e51b75eea3d12e2e7558f24d1ba71bce26aabaac524dd9caa4ba17eb7bb64ca7.jpg",
+              'Tom Hanks': 'https://pmcvariety.files.wordpress.com/2016/09/tom-hanks.jpg?w=670&h=377&crop=1',
+              'Will Smith': 'https://static.independent.co.uk/s3fs-public/styles/story_medium/public/thumbnails/image/2015/10/02/12/Will-Smith-Fresh-Prince.jpg',
+              'Robbin Williams': 'http://static.parade.com/wp-content/uploads/2013/09/robin-williams-cover-ftr.jpg',
+              'George Clooney': 'http://vignette2.wikia.nocookie.net/muppet/images/c/c8/George_Clooney.jpg/revision/latest?cb=20151209144015',
+              'Matt Damon': 'http://cp91279.biography.com/1000509261001/1000509261001_2036180457001_BIO-Biography-Matt-Damon-LF.jpg',
+              'Nicolas Cage': 'https://discdoesretro.files.wordpress.com/2013/07/tumblr_mftalf5cpv1qa934fo1_1280.png',
+              'Woody Allen': 'http://www.woodyallenpages.com/wp-content/uploads/2015/08/woodyportrait-xlarge.jpg',
+              'Anthony Hopkins': 'http://img.cinemablend.com/cb/e/5/e/d/1/9/e5ed19acebd1505b5e5f94a4145ab27913521cfa4c3f4b18f72fefd8dc8cdb9f.jpg',
+              'Gary Oldman': 'http://www.shauntmax30.com/data/out/15/1059195-photo-for-desktop-gary-oldman.jpg',
+              'Samuel L Jackson': 'http://cdn1-www.craveonline.com/assets/uploads/2015/06/Samuel-L-Jackson-Pulp-Fiction.jpg',
+              'Antonio Banderas': 'http://www.hotflick.net/flicks/1995_Desperado/big/fhd995PDO_Antonio_Banderas_013.jpg',
+              'John Wayne': 'http://cdn.images.express.co.uk/img/dynamic/79/590x/11f38wayne1-469761.jpg'}
+
     file = open(path, 'r')
     lines = file.readlines()
+    file.close()
     tickers, amounts, clients, seconds, directions = [], [], [], [], []
 
+    line_num = 0
     for line in lines:
-        tickersPat = re.compile("ticker: '(.*)', amount")
-        tickers.append(tickersPat.findall(line)[0])
-        amountsPat = re.compile("amount: '(.*)', client")
-        amounts.append(amountsPat.findall(line)[0])
-        clientsPat = re.compile("client: '(.*)', second")
-        clients.append(clientsPat.findall(line)[0])
-        secondsPat = re.compile("second: '(.*)', direction")
-        seconds.append(secondsPat.findall(line)[0])
-        directionsPat = re.compile("direction: '(.*)'")
-        directions.append(directionsPat.findall(line)[0])
-        lineNum += 1
+        tickers_pat = re.compile("ticker: '(.*)', amount")
+        tickers.append(tickers_pat.findall(line)[0])
+        amounts_pat = re.compile("amount: '(.*)', client")
+        amounts.append(amounts_pat.findall(line)[0])
+        clients_pat = re.compile("client: '(.*)', second")
+        clients.append(clients_pat.findall(line)[0])
+        seconds_pat = re.compile("second: '(.*)', direction")
+        seconds.append(seconds_pat.findall(line)[0])
+        directions_pat = re.compile("direction: '(.*)'")
+        directions.append(directions_pat.findall(line)[0])
+        line_num += 1
 
-    transactions = []
-    for i in range(0, lineNum):
-        transactions.append(Transaction(tickers[i], amounts[i], clients[i], seconds[i], directions[i]))
+    for tx in zip(tickers, amounts, clients, seconds, directions):
+        transactions.append(Transaction(tx))
 
-    def findTopInvestor(self):
-        maximum = 0
+    def random_actor(self):
+        actor_id = random.randrange(len(self.actors))
+        actor_name = None
+        for _id, actor in enumerate(self.actors.keys()):
+            if _id == actor_id:
+                actor_name = actor
+                break
+        return actor_name
+
+    @staticmethod
+    def amount_to_int(amount):
+        return int(amount[1:-3].replace(',', '').replace('.', ''))
+
+    @staticmethod
+    def int_to_amount(amount):
+        result = '00.'
+        for i, symbol in enumerate(reversed(str(amount))):
+            result += symbol
+            if i % 3 == 2:
+                result += ','
+        result += '€'
+        return result[::-1]
+
+    def richest_investor(self):
+        maximum, top_investor = 0, None
         for transaction in self.transactions:
-            current_amount = transaction.amount[1:-3]
-            current_amount = current_amount.replace(",", "")
-            current_amount = int(current_amount.replace(".", ""))
+            current_amount = self.amount_to_int(transaction.amount)
             if maximum < current_amount:
                 maximum = current_amount
                 top_investor = transaction.client
         return top_investor
 
-    def returnRand(self):
-        rand = random.randrange(0, len(self.transactions))
-        return rand
+    def random_tx(self):
+        return self.transactions[random.randrange(0, len(self.transactions))]
 
-    def returnMostSuAdvisor(self):
-        maxAppearances = 0
-        for transaction1 in self.transactions:
-            appearances = 0
-            for transaction2 in self.transactions:
-                if transaction1.second == transaction2.second:
-                    appearances += 1
-            if appearances > maxAppearances:
-                maxAppearances = appearances
-                toReturn = transaction1.second
-        return toReturn
+    def most_active_advisor(self):
+        num_of_deals, advisor_name = 0, None
+        for tx in self.transactions:
+            _num_of_deals = self.num_of_entries_advisor(tx.second)
+            if _num_of_deals > num_of_deals:
+                num_of_deals = _num_of_deals
+                advisor_name = tx.second
+        return advisor_name
 
-    def BradPitt(self):
-        appearances = 0
-        for transaction in self.transactions:
-            if transaction.client == "Brad Pitt":
-                appearances += 1
-        return appearances
+    def invested_money(self, name):
+        return sum(self.amount_to_int(tx.amount) for tx in self.transactions if tx.client == name and tx.direction == 'RED')
 
-    def randomiseAnswers(self, goodAnswer, badAnswer):
-        ans0 = []
-        ans0.append(goodAnswer)
-        ans0.append(badAnswer)
-        correct = 0
-        if random.randrange(0, 2) == 0:
-            helpingHand = ans0[0]
-            ans0[0] = ans0[1]
-            ans0[1] = helpingHand
-            correct = 1
-        return [ans0[0], ans0[1], correct]
+    def withdrawn_money(self, name):
+        return sum(self.amount_to_int(tx.amount) for tx in self.transactions if tx.client == name and tx.direction == 'SUB')
+
+    def num_of_entries_advisor(self, name):
+        return sum(1 for tx in self.transactions if tx.second == name)
+
+    def number_of_entries_client(self, name):
+        return sum(1 for tx in self.transactions if tx.client == name)
+
+    @staticmethod
+    def randomise_answers(good_answer, bad_answer):
+        ans = [good_answer, bad_answer]
+        r = random.randrange(0, 2) == 0
+        correct = 0 if r else 1
+        return [ans[0] if r else ans[1], ans[1] if r else ans[0], correct]
 
     def next_question(self):
-        q = []
+        question_type = random.randrange(0, 100)
+        ans = None, None
+        if question_type == 0:
+            while ans[0] == ans[1]:
+                ans = self.randomise_answers(self.richest_investor(), self.random_tx().client)
+            link = "http://pngimg.com/upload/money_PNG3545.png"
+            question_text = "Who is the guy who invested more money than others until now?"
+        elif question_type == 1:
+            while ans[0] == ans[1]:
+                ans = self.randomise_answers(self.most_active_advisor(), self.random_tx().second)
+            link = "http://inspiregroup.ro/wp-content/uploads/2015/08/success-1.jpg"
+            question_text = "Which advisor managed more deals than others?"
+        elif question_type < 30:
+            tx = self.random_tx()
+            while ans[0] == ans[1]:
+                ans = self.randomise_answers(tx.second, self.random_tx().client)
+            link = "http://assets.howtobecome.com/assets/images/2014/03/Financial-Advisor.jpg"
+            question_text = "Who is the financial advisor of {}?".format(tx.client)
+        elif question_type < 50:
+            tx = self.random_tx()
+            while ans[0] == ans[1]:
+                real_amount = self.int_to_amount(self.withdrawn_money(tx.client))
+                ans = self.randomise_answers(real_amount, self.random_tx().amount)
+            link = self.actors.get(tx.client, "http://assets.howtobecome.com/assets/images/2014/03/Financial-Advisor.jpg")
+            question_text = "How much money did {} withdraw?".format(tx.client)
+        elif question_type < 75:
+            tx = self.random_tx()
+            while ans[0] == ans[1]:
+                real_amount = self.int_to_amount(self.invested_money(tx.client))
+                ans = self.randomise_answers(real_amount, self.random_tx().amount)
+            link = self.actors.get(tx.client, "http://assets.howtobecome.com/assets/images/2014/03/Financial-Advisor.jpg")
+            question_text = "How much money did {} invest?".format(tx.client)
+        else:
+            actor_name = self.random_actor()
+            while ans[0] == ans[1]:
+                print('actor: {}, times: {}'.format(actor_name, self.number_of_entries_client(actor_name)))
+                ans = self.randomise_answers(self.number_of_entries_client(actor_name), random.randrange(0, 10))
+            print(ans)
+            ans = '{} times'.format(ans[0]), '{} times'.format(ans[1]), ans[2]
+            link = self.actors[actor_name]
+            question_text = "How many times does {} appear in our transaction data?".format(actor_name)
+        return link, question_text, ans[0], ans[1], ans[2]
 
-        ans0 = self.randomiseAnswers(self.findTopInvestor(), self.transactions[self.returnRand()].client)
-        link0 = "http://pngimg.com/upload/money_PNG3545.png"
-        q.append((link0, "Who is the guy who invested more money than others until now?", ans0[0], ans0[1], ans0[2]))
 
-        transaction1 = self.transactions[self.returnRand()]
-        ans1 = self.randomiseAnswers(transaction1.second, self.transactions[self.returnRand()].client)
-        link1 = "http://assets.howtobecome.com/assets/images/2014/03/Financial-Advisor.jpg"
-        q.append((link1, "Who is the financial advisor of " + transaction1.client + " ?", ans1[0], ans1[1], ans1[2]))
-
-        ans2 = self.randomiseAnswers(self.returnMostSuAdvisor(), self.transactions[self.returnRand()].second)
-        link2 = "http://inspiregroup.ro/wp-content/uploads/2015/08/success-1.jpg"
-        q.append((link2, "Who is the most succesfull advisor?", ans2[0], ans2[1], ans2[2]))
-
-        ans3 = self.randomiseAnswers(self.BradPitt(), random.randrange(0, 10))
-        if ans3 == 2:  # because actually 2 is the correct answer
-            ans3 = 4
-        link3 = "https://media1.popsugar-assets.com/files/thumbor/9xeS3fpQc1QdvdtpD5I4grLYMI4/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2012/09/36/2/192/1922398/2540217bec064a5c_71841717_10/i/Brad-Pitt-got-handsy-2006-press-conference-Babel.jpg"
-        q.append((link3, "How many times does Brad Pitt appear in our transaction data?", '{} times'.format(ans3[0]),
-                  '{} times'.format(ans3[1]), ans3[2]))
-        return q[random.randrange(0, len(q))]
-9
 a = Data()
-print(a.next_question())
+print(a.number_of_entries_client('Nicolas Cage'))
