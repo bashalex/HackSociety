bd7902fe9fab52ad215d61f1f8f0877bebc58a86
Alex Bash <avbashlykov@gmail.com>
2016-12-10 01:30:58 +0300

    new images added

--- questions.py
+++ questions.py
@@ -27,10 +27,19 @@ class Data:
               'Nicolas Cage': 'https://discdoesretro.files.wordpress.com/2013/07/tumblr_mftalf5cpv1qa934fo1_1280.png',
               'Woody Allen': 'http://www.woodyallenpages.com/wp-content/uploads/2015/08/woodyportrait-xlarge.jpg',
               'Anthony Hopkins': 'http://img.cinemablend.com/cb/e/5/e/d/1/9/e5ed19acebd1505b5e5f94a4145ab27913521cfa4c3f4b18f72fefd8dc8cdb9f.jpg',
-              'Gary Oldman': 'http://www.shauntmax30.com/data/out/15/1059195-photo-for-desktop-gary-oldman.jpg',
+              'Gary Oldman': 'https://saymber.files.wordpress.com/2015/10/bram-stoker-s-dracula-bram-stokers-dracula-gary-oldman.jpg?w=1024',
               'Samuel L Jackson': 'http://cdn1-www.craveonline.com/assets/uploads/2015/06/Samuel-L-Jackson-Pulp-Fiction.jpg',
               'Antonio Banderas': 'http://www.hotflick.net/flicks/1995_Desperado/big/fhd995PDO_Antonio_Banderas_013.jpg',
-              'John Wayne': 'http://cdn.images.express.co.uk/img/dynamic/79/590x/11f38wayne1-469761.jpg'}
+              'John Wayne': 'http://cdn.images.express.co.uk/img/dynamic/79/590x/11f38wayne1-469761.jpg',
+              'Joseph Fiennes ': 'http://www.christianitytoday.com/images/67668.jpg?w=640',
+              'Ben Kingsley': 'http://resizing.flixster.com/vKmH_1OS3Mm3RBPnToapsISpHuI=/700x380/dkpu1ddg7pbsk.cloudfront.net/site/10/27/28/10272876_ori.jpg',
+              'Robert Redford': 'http://cp91279.biography.com/Robert-Redford_Early-Life_HD_768x432-16x9.jpg',
+              'Sylvester Stallone': 'http://images.indianexpress.com/2016/01/sylvester-stallone-759.jpg',
+              'Paul Newman': 'http://www.bestmoviesbyfarr.com.s3.amazonaws.com/articles/5ppqsS/images/2-1jjgrhu.jpg',
+              'Roger Moore': 'http://www.007.com/wp-content/uploads/2014/01/Roger-Moore-james-bond-BW.jpg',
+              'John Goodman': 'http://i.onionstatic.com/avclub/5568/96/16x9/960.jpg',
+              'Gene Hackman': 'http://www.bestmoviesbyfarr.com/static-assets/images/articles/background/2016/01/gene-hackman-big.jpeg',
+              'Mickey Rourke': 'http://www.hobbyconsolas.com/sites/hobbyconsolas.com/public/media/image/2015/02/448068-cine-superheroes-critica-iron-man-2.png'}
 
     file = open(path, 'r')
     lines = file.readlines()
@@ -38,7 +47,7 @@ class Data:
     tickers, amounts, clients, seconds, directions = [], [], [], [], []
 
     line_num = 0
-    for line in lines:
+    for i, line in enumerate(lines):
         tickers_pat = re.compile("ticker: '(.*)', amount")
         tickers.append(tickers_pat.findall(line)[0])
         amounts_pat = re.compile("amount: '(.*)', client")
@@ -72,7 +81,7 @@ class Data:
         result = '00.'
         for i, symbol in enumerate(reversed(str(amount))):
             result += symbol
-            if i % 3 == 2:
+            if i % 3 == 2 and i != len(str(amount)) - 1:
                 result += ','
         result += '€'
         return result[::-1]
@@ -134,7 +143,7 @@ class Data:
             tx = self.random_tx()
             while ans[0] == ans[1]:
                 ans = self.randomise_answers(tx.second, self.random_tx().client)
-            link = "http://assets.howtobecome.com/assets/images/2014/03/Financial-Advisor.jpg"
+            link = self.actors.get(tx.client, "http://assets.howtobecome.com/assets/images/2014/03/Financial-Advisor.jpg")
             question_text = "Who is the financial advisor of {}?".format(tx.client)
         elif question_type < 50:
             tx = self.random_tx()
